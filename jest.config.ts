756cc4a926e4d755e4e02c3e446f875575081029
Alex Hall <alexpeterhall@gmail.com>
2023-06-28 10:33:19 -0400

    Add Jest

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+// ./jest.config.ts
+import type { Config } from '@jest/types'
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: [".d.ts", ".js"],
+  verbose: true,
+}
+export default config
