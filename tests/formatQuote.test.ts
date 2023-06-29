5a9abc9a70d069c744d9b4417645535e43db0ffe
Alex Hall <alexpeterhall@gmail.com>
2023-06-28 20:41:59 -0400

    Move test data to its own file

--- tests/formatQuote.test.ts
+++ tests/formatQuote.test.ts
@@ -1,37 +1,22 @@
-import {formatQuote} from '../quotes'
+import { formatQuote } from '../quotes'
+import { testQuotes } from './testData'
 
 describe('Sunny Day Scenarios', () => {
   test('Quote with no tags formats properly', () => {
-    expect(formatQuote({
-      "quote": "This quote has no tags.",
-      "author": "Testy McTester",
-      "tags": []
-    })).toBe("This quote has no tags. - Testy McTester")
+    expect(formatQuote(testQuotes[0])).toBe("This quote has no tags. - Testy McTester")
   })
 
   test('Quote with one tag formats properly', () => {
-    expect(formatQuote(    {
-      "quote": "This quote has one tag.",
-      "author": "Testy McTester",
-      "tags": ["thisIsATag"]
-    })).toBe("This quote has one tag. - Testy McTester - #thisIsATag")
+    expect(formatQuote(testQuotes[1])).toBe("This quote has one tag. - Testy McTester - #thisIsATag")
   })
 
   test('Quote with multiple tags formats properly', () => {
-    expect(formatQuote({
-      "quote": "This quote has multiple tags.",
-      "author": "Testy McTester",
-      "tags": ["thisIsATag", "thisIsAnotherTag"]
-    })).toBe("This quote has multiple tags. - Testy McTester - #thisIsATag #thisIsAnotherTag")
+    expect(formatQuote(testQuotes[2])).toBe("This quote has multiple tags. - Testy McTester - #thisIsATag #thisIsAnotherTag #thisIsYetAnotherTag")
   })
 })
 
 describe('Rainy Day Scenarios', () => {
   test('Quote with duplicate tags shows the tag only once', () => {
-    expect(formatQuote({
-      "quote": "This quote has duplicate tags.",
-      "author": "Testy McTester",
-      "tags": ["thisIsATag", "thisIsATag"]
-    })).toBe("This quote has duplicate tags. - Testy McTester - #thisIsATag")
+    expect(formatQuote(testQuotes[3])).toBe("This quote has duplicate tags. - Testy McTester - #thisIsATag")
   })
 })
---
+++ tests/testData.ts
@@ -0,0 +1,23 @@
+//! DO NOT CHANGE THE ORDER OF THIS DATA
+export const testQuotes = [
+  {
+    quote: 'This quote has no tags.',
+    author: 'Testy McTester',
+    tags: [],
+  },
+  {
+    quote: 'This quote has one tag.',
+    author: 'Testy McTester',
+    tags: ['thisIsATag'],
+  },
+  {
+    quote: 'This quote has multiple tags.',
+    author: 'Testy McTester',
+    tags: ['thisIsATag', 'thisIsAnotherTag', 'thisIsYetAnotherTag'],
+  },
+  {
+    quote: 'This quote has duplicate tags.',
+    author: 'Testy McTester',
+    tags: ['thisIsATag', 'thisIsATag'],
+  }
+]
